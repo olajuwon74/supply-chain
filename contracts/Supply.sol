86b1b3ecc9924aa89c10dc10229f2559be737bc8
Solomon <adebayosolomon74@gmail.com>
2022-06-04 10:59:42 +0100

    Gas optimization

--- contracts/Supply.sol
+++ contracts/Supply.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.7;
 
 contract Supply{
 
-    address immutable general = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-    address superAdmin;
+    // address immutable general = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address public superAdmin;
 
     error NotApproved();
 
